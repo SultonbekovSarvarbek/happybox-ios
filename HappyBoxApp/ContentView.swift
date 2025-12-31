f89ae40b4d6eb73b0f0008e81d816462e0a0f99c
Sarvarbek <sarvarsarvar944@gmail.com>
2026-01-01 04:02:50 +0500

    Initial Commit

---
+++ HappyBoxApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HappyBoxApp
+//
+//  Created by Sultonbekov Sarvar on 01/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HappyBoxApp/HappyBoxAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyBoxAppApp.swift
+//  HappyBoxApp
+//
+//  Created by Sultonbekov Sarvar on 01/01/26.
+//
+
+import SwiftUI
+
+@main
+struct HappyBoxAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
